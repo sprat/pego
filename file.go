5c3e8e1a9cb32304f6c3392418e967c08398067e
Sylvain Prat <sylvain.prat@achird.net>
2026-01-17 19:02:20 +0100

    Simplify the code by creating the PE instance at the start of the NewPE function

--- file.go
+++ file.go
@@ -16,54 +16,45 @@ type PE struct {
 
 // NewPE creates a PE instance
 func NewPE(reader io.ReaderAt) (*PE, error) {
-	var dosStub *Segment
-	var peSignature *Header[PESignature]
+	p := PE{}
 	offset := int64(0)
 
 	// DOS Header
 	dosHeader, err := NewHeader[DOSHeader](reader, &offset)
-	if err != nil {
-		return nil, err
-	}
+	if err == nil && dosHeader.Data.Magic == 0x5a4d {
+		p.DOSHeader = dosHeader
 
-	if dosHeader.Data.Magic == 0x5a4d {
 		// DOS Stub
 		peHeaderOffset := int64(dosHeader.Data.Lfanew)
 		dosStubSize := peHeaderOffset - offset
-		dosStub = NewSegment(reader, &offset, dosStubSize)
+		p.DOSStub = NewSegment(reader, &offset, dosStubSize)
 
 		// PE Signature
-		peSignature, err = NewHeader[PESignature](reader, &offset)
+		p.PESignature, err = NewHeader[PESignature](reader, &offset)
 		if err != nil {
 			return nil, err
 		}
-		if peSignature.Data != 0x00004550 {
-			return nil, fmt.Errorf("invalid PE file signature: %#x", peSignature.Data)
+		if p.PESignature.Data != 0x00004550 {
+			return nil, fmt.Errorf("invalid PE file signature: %#x", p.PESignature.Data)
 		}
 	} else {
 		offset = 0
-		dosHeader = nil
 	}
 
 	// COFF Header
-	coffHeader, err := NewHeader[pe.FileHeader](reader, &offset)
+	p.COFFHeader, err = NewHeader[pe.FileHeader](reader, &offset)
 	if err != nil {
 		return nil, err
 	}
 
 	// make sure the machine type is valid
-	if !isValidMachine(coffHeader.Data.Machine) {
-		return nil, fmt.Errorf("unrecognized PE machine: %#x", coffHeader.Data.Machine)
+	if !isValidMachine(p.COFFHeader.Data.Machine) {
+		return nil, fmt.Errorf("unrecognized PE machine: %#x", p.COFFHeader.Data.Machine)
 	}
 
 	// TODO: add protections to defend against malicious files (e.g. oversized segments...)
 
-	return &PE{
-		DOSHeader:   dosHeader,
-		DOSStub:     dosStub,
-		PESignature: peSignature,
-		COFFHeader:  coffHeader,
-	}, nil
+	return &p, nil
 }
 
 func isValidMachine(machine uint16) bool {
