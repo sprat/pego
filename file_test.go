0925746e6391ca8ed8189748ff051cc06b9e41f6
Sylvain Prat <sylvain.prat@achird.net>
2026-01-17 19:40:58 +0100

    Make the file test less verbose

--- file_test.go
+++ file_test.go
@@ -14,24 +14,24 @@ func TestFileReadExe(t *testing.T) {
 	assert.NilError(t, err)
 	defer file.Close()
 
-	peFile, err := NewPE(file)
+	p, err := NewPE(file)
 	assert.NilError(t, err)
 
 	// DOS Header
-	assert.Equal(t, peFile.DOSHeader.Size(), int64(64))
-	assert.Equal(t, peFile.DOSHeader.Data.Lfanew, uint32(64+128))
+	assert.Equal(t, p.DOSHeader.Size(), int64(64))
+	assert.Equal(t, p.DOSHeader.Data.Lfanew, uint32(64+128))
 
 	// DOS Stub
-	assert.Equal(t, peFile.DOSStub.Size(), int64(128))
+	assert.Equal(t, p.DOSStub.Size(), int64(128))
 
 	// PE Signature
-	assert.Assert(t, peFile.PESignature != nil)
+	assert.Assert(t, p.PESignature != nil)
 
 	// COFF Header
-	assert.Equal(t, peFile.COFFHeader.Size(), int64(20))
-	assert.Equal(t, peFile.COFFHeader.Data.Machine, uint16(pe.IMAGE_FILE_MACHINE_I386))
-	assert.Equal(t, peFile.COFFHeader.Data.NumberOfSections, uint16(3))
-	assert.Equal(t, peFile.COFFHeader.Data.SizeOfOptionalHeader, uint16(224))
+	assert.Equal(t, p.COFFHeader.Size(), int64(20))
+	assert.Equal(t, p.COFFHeader.Data.Machine, uint16(pe.IMAGE_FILE_MACHINE_I386))
+	assert.Equal(t, p.COFFHeader.Data.NumberOfSections, uint16(3))
+	assert.Equal(t, p.COFFHeader.Data.SizeOfOptionalHeader, uint16(224))
 }
 
 func TestFileReadObj(t *testing.T) {
@@ -39,17 +39,17 @@ func TestFileReadObj(t *testing.T) {
 	assert.NilError(t, err)
 	defer file.Close()
 
-	peFile, err := NewPE(file)
+	p, err := NewPE(file)
 	assert.NilError(t, err)
 
 	// DOS Header, DOS Stub and PE Signature should not by present
-	assert.Assert(t, peFile.DOSHeader == nil)
-	assert.Assert(t, peFile.DOSStub == nil)
-	assert.Assert(t, peFile.PESignature == nil)
+	assert.Assert(t, p.DOSHeader == nil)
+	assert.Assert(t, p.DOSStub == nil)
+	assert.Assert(t, p.PESignature == nil)
 
 	// COFF Header
-	assert.Equal(t, peFile.COFFHeader.Size(), int64(20))
-	assert.Equal(t, peFile.COFFHeader.Data.Machine, uint16(pe.IMAGE_FILE_MACHINE_AMD64))
-	assert.Equal(t, peFile.COFFHeader.Data.NumberOfSections, uint16(6))
-	assert.Equal(t, peFile.COFFHeader.Data.SizeOfOptionalHeader, uint16(0))
+	assert.Equal(t, p.COFFHeader.Size(), int64(20))
+	assert.Equal(t, p.COFFHeader.Data.Machine, uint16(pe.IMAGE_FILE_MACHINE_AMD64))
+	assert.Equal(t, p.COFFHeader.Data.NumberOfSections, uint16(6))
+	assert.Equal(t, p.COFFHeader.Data.SizeOfOptionalHeader, uint16(0))
 }
